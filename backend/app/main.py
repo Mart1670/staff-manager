fa541f2ba15d8ad7baf1cde05946b15818c0c555
martin.harlaut <119311227+Mart1670@users.noreply.github.com>
2025-02-20 00:00:42 +0100

    Create first route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
+
+router = APIRouter()
+
+@router.get("/")
+async def home():
+    return ("Welcome home !")
+
+app.include_router(router)
 
 def start():
     """Launched with 'poetry run start' at root level"""
